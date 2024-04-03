db889868295c103d1d4e4d1e5bbe5721ff12b368
joaobaptista1987 <202301121@estudantes.ips.pt>
2024-04-03 08:53:51 +0100

    Nível 1

---
+++ AirTransportation.java
@@ -0,0 +1,24 @@
+
+/**
+ * Escreva uma descrição da classe AirTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class AirTransportation extends Transport
+{
+    // variáveis de instância 
+    private String name;
+    private int numberOfContainers;
+    private final double feeAirTransportation = 0.04;
+
+    /**
+     * Construtor para objetos da classe AirTransportation
+     */
+    public AirTransportation()
+    {
+        
+    }
+
+   
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,25 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport
+{
+    
+    private String licensePlate;
+    private final double feeGroundTransportation = 0.03;
+  
+
+    /**
+     * Construtor para objetos da classe GroundTransportation
+     */
+    public GroundTransportation()
+    {
+        // inicializa variáveis de instância
+        
+    }
+
+    
+}
